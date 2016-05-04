730ab6ecfb05ceb261755f2911385157bd35febe
nicolasjhampton <nicolasjhampton@gmail.com>
2016-05-03 21:44:27 -0700

    Private/public concerns separated

--- js/quoter.js
+++ js/quoter.js
@@ -3,6 +3,13 @@ var Quoter = (function($) {
 
   var autoPlay;
   var usedQuotes = [];
+  var colors;
+  var quotes;
+  var quoteClass;
+  var quoteBox;
+  var billboard;
+  var title;
+  var quoteClassString;
 
   // Templates for various quote elements
   var templates =
@@ -43,6 +50,13 @@ var Quoter = (function($) {
     return templates[key].begin + content + templates[key].end;
   };
 
+  // Sets a random color based on the color object
+  var setRandomColor = function() {
+    //var that = this;
+    var randomIndex = random(0, colors.length);
+    billboard.css("background-color", colors[randomIndex]);
+  };
+
   // assembles the html string for each element from the template array
   var makeElement = function(obj, key) {
 
@@ -61,76 +75,87 @@ var Quoter = (function($) {
     return template(key, content);
   };
 
+  // returns a random quote object from the quotes array
+  var getRandomQuote = function() {
+    var randomIndex = random(0, quotes.length);
+    if (usedQuotes.indexOf(randomIndex) === -1) { // If we haven't used this quote before...
+      usedQuotes.push(randomIndex); // Add it to the list of used quotes
+      return quotes[randomIndex]; // Return the quote
+    } else if (usedQuotes.length === quotes.length) {
+      usedQuotes = []; // Reset the list of used quotes if we've used them all
+    }
+    //Rerun this function if we used this quote before
+    return getRandomQuote(); // Recursion like woah ;)
+  };
 
-  function Quoter(quotes, config) {
+
+  function Quoter(quoteObject, config) {
     if(!config) { config = {}; }
-    this.quotes = quotes;
-    this.quoteBox = config.quoteBox || $('#quote-box');
-    this.body = config.body || $('body');
-    this.colors = config.colors || [
+    quotes = quoteObject;
+    title = config.title || "";
+    quoteClass = config.quoteClass || 'quote-box'
+    quoteClassString = '.' + quoteClass;
+    colors = config.colors || [
       "blue",
       "red",
       "green",
       "purple"
     ];
-
   }
 
-  Quoter.prototype.printQuote = function() {
+  Quoter.prototype.printQuotes = function() {
     var that = this;
     var quoteHTML = "";
     var sourceHTML = "";
-    var randomQuote = that.getRandomQuote(); // Get a random quote
+    var randomQuote = getRandomQuote(); // Get a random quote
     //  Hide the quoteBox, then clear the inside of it
-    that.quoteBox.hide();
-    that.quoteBox.children().remove();
+    quoteBox.hide();
+    quoteBox.children().remove();
     // Get all the keynames in an array, then map through them
     Object.keys(randomQuote).map(function(key, index){
       // Append first 2 to the quoteBox, the rest go inside the source element
-      var target = index < 2 ? that.quoteBox : that.quoteBox.children('.source');
+      var target = index < 2 ? quoteBox : quoteBox.children('.source');
       target.append(makeElement(randomQuote, key));
     });
     // Randomly change the color of the screen and fade it in
-    that.setRandomColor();
-    that.quoteBox.fadeIn('slow');
+    setRandomColor();
+    quoteBox.fadeIn('slow');
     // If an autotimer has been set, clear and reset it
     if(this.duration) {
-      window.clearTimeout(autoPlay);
-      autoPlay = window.setTimeout(that.printQuote.bind(that), this.duration);
+      if(autoPlay) {window.clearTimeout(autoPlay);}
+      autoPlay = window.setInterval(that.printQuotes.bind(that), this.duration);
     }
   }
 
-  // Sets a random color based on the color object
-  Quoter.prototype.setRandomColor = function() {
+  Quoter.prototype.attachTo = function(cssSelector) {
     var that = this;
-    var randomIndex = random(0, that.colors.length);
-    this.body.css("background-color", that.colors[randomIndex]);
-  };
+    billboard = $(cssSelector);
+    billboard.css({
+      "position": "absolute",
+      "display": "inline-block",
+      "height": "100%",
+      "width": "100%"
+    });
+    if (billboard.children('.' + quoteClass).length === 0) {
+      var quoteStart = $('<div class="' + quoteClass + '"></div>')
+                        .append($('<p class="title">' + title + '</p>'));
+      billboard.prepend(quoteStart);
 
-
-  // returns a random quote object from the quotes array
-  Quoter.prototype.getRandomQuote = function() {
-    var randomIndex = random(0, this.quotes.length);
-    if (usedQuotes.indexOf(randomIndex) === -1) { // If we haven't used this quote before...
-      usedQuotes.push(randomIndex); // Add it to the list of used quotes
-      return this.quotes[randomIndex]; // Return the quote
-    } else if (usedQuotes.length === this.quotes.length) {
-      usedQuotes = []; // Reset the list of used quotes if we've used them all
     }
-    //Rerun this function if we used this quote before
-    return this.getRandomQuote(); // Recursion like woah ;)
-  };
-
-  Quoter.prototype.attachButton = function ($object) {
-    var that = this;
-    $object.click(that.printQuote.bind(that));
+    quoteBox = $(quoteClassString);
+    window.setTimeout(that.printQuotes.bind(that), 5000);
     return that; // Chainable
   }
 
-  Quoter.prototype.setAutoplay = function (duration) {
+  Quoter.prototype.setButton = function(cssSelector) {
+    var that = this;
+    $(cssSelector).click(that.printQuotes.bind(that));
+    return that; // Chainable
+  }
+
+  Quoter.prototype.setAutoplay = function(duration) {
     var that = this;
     this.duration = duration;
-    autoPlay = window.setTimeout(that.printQuote.bind(that), duration);
     return that; // Chainable
   }
 
--- js/quotes.js
+++ js/quotes.js
@@ -24,3 +24,26 @@ var starWarsQuotes =
       "tags": "humor art"
     }
   ];
+
+  var starWarsQuotes2 =
+    [
+      {
+        "quote": "I am a Jedi, like my father before me.",
+        "source": "Luke Skywalker",
+        "citation": "Return of the Jedi",
+        "year": 1984
+      },
+      {
+        "quote": "I am your father.",
+        "source": "Darth Vader",
+        "citation": "The Empire Strikes Back",
+        "tags": "humor science"
+      },
+      {
+        "quote": "He's my brother.",
+        "source": "Princess Leia",
+        "citation": "Return of the Jedi",
+        "year": 1984,
+        "tags": "humor art"
+      }
+    ];
